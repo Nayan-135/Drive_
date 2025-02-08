6c89bdac957db2f5d88d0b6c292f241924393844
Nayan Ghate <ghatenayan5@gmail.com>
2025-02-08 13:17:44 +0530

    Add files via upload

---
+++ routes/user.routes.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const { body, validationResult } = require('express-validator');
+const router = express.Router();
+const userModel= require('../models/user.model')
+const bcrypt = require('bcrypt');
+const  jwt = require('jsonwebtoken');
+
+
+router.get('/register',(req,res)=>{
+    res.render('register');
+})
+router.post('/register',
+    body('email').trim().isEmail(),
+    body('password').trim().isLength({ min: 5 }),
+    body('username').trim().isLength({min:3}),
+    async(req,res)=>{
+
+                const errors =validationResult(req);
+                if(!errors.isEmpty())
+                {
+                    return res.status(400).json({
+                        errors: errors.array(),
+                        message:  'Invalid input'
+
+                    })
+                }
+                const {email,username,password}= req.body;
+                const hashpassword = await bcrypt.hash(password,10)
+                const newUser = await userModel.create({
+                    email,
+                    username,
+                    password : hashpassword
+                })
+                res.json(newUser)
+
+})
+
+router.get('/login',(req,res)=>{
+    res.render('login')
+})
+router.post('/login',
+    body('username').trim().isLength({min:3}),
+    body('password').trim().isLength({min:5}),
+    async(req,res)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                error:errors.array(),
+                message: 'Invalid input'
+        })
+    }
+    const {username,password}=req.body;
+    const user = await userModel.findOne({username:username});
+    if(!user){
+        return res.status(400).json({
+            message: 'Invalid username or password',
+        })
+    }
+    const isMatch = await bcrypt.compare(password,user.password)
+    if(!isMatch)
+    {
+        return res.status(400).json({
+            message: 'Invalid username or password',
+        })
+    }
+    const token = jwt.sign({
+        userId:user._id,
+        email:user.email,
+        username:user.username
+    },process.env.JWT_SECRET)
+    res.cookie('token',token)
+    res.send('loged in')
+    
+})
+
+
+module.exports = router;
